cc8d3baca545cf9ad453ab3b10de435cdfa53f4b
n20va <99407852+n20va@users.noreply.github.com>
2025-09-05 21:51:44 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,13 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Data
 public class User {
     private int id;
+    
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
     private String email;
+    
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
     private String login;
+    
     private String name;
+    
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 }
