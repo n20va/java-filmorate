ae4cfa50ce195ad4b612fcbc4902d05d60eae9d5
n20va <99407852+n20va@users.noreply.github.com>
2025-09-05 21:58:43 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -11,17 +11,14 @@ import java.time.LocalDate;
 @Data
 public class User {
     private int id;
-    
     @NotBlank(message = "Email не может быть пустым")
     @Email(message = "Некорректный формат email")
     private String email;
-    
     @NotBlank(message = "Логин не может быть пустым")
     @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
     private String login;
-    
     private String name;
-    
     @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 }
+
