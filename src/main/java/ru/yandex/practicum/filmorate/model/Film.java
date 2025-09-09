cf18495135feb08d8b6896fa6b738b7b718bd855
n20va <99407852+n20va@users.noreply.github.com>
2025-09-09 22:32:26 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,26 +4,27 @@ import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Objects;
 
 public class Film {
     private int id;
-    
+
     @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
-    
+
     @Size(max = 200, message = "Максимальная длина описания — 200 символов")
     private String description;
-    
+
     private LocalDate releaseDate;
-    
+
     @Positive(message = "Продолжительность фильма должна быть положительным числом")
     private int duration;
-    
+
     private Set<Integer> likes = new HashSet<>();
-    
+
     public Film() {
     }
-    
+
     public Film(int id, String name, String description, LocalDate releaseDate, int duration) {
         this.id = id;
         this.name = name;
@@ -31,67 +32,67 @@ public class Film {
         this.releaseDate = releaseDate;
         this.duration = duration;
     }
-    
+
     public int getId() {
         return id;
     }
-    
+
     public void setId(int id) {
         this.id = id;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public String getDescription() {
         return description;
     }
-    
+
     public void setDescription(String description) {
         this.description = description;
     }
-    
+
     public LocalDate getReleaseDate() {
         return releaseDate;
     }
-    
+
     public void setReleaseDate(LocalDate releaseDate) {
         this.releaseDate = releaseDate;
     }
-    
+
     public int getDuration() {
         return duration;
     }
-    
+
     public void setDuration(int duration) {
         this.duration = duration;
     }
-    
+
     public Set<Integer> getLikes() {
         return likes;
     }
-    
+
     public void setLikes(Set<Integer> likes) {
         this.likes = likes;
     }
-    
+
     public void addLike(int userId) {
         likes.add(userId);
     }
-    
+
     public void removeLike(int userId) {
         likes.remove(userId);
     }
-    
+
     public int getLikesCount() {
         return likes.size();
     }
-    
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -99,12 +100,12 @@ public class Film {
         Film film = (Film) o;
         return id == film.id;
     }
-    
+
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
-    
+
     @Override
     public String toString() {
         return "Film{" +
