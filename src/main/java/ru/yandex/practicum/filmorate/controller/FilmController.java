7706c51c4278bce1a722493e8dde54d707973ef6
n20va <99407852+n20va@users.noreply.github.com>
2025-09-09 21:57:47 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
-import ru.yandex.practicum.filmorate.exception.ValidationException; 
-import java.time.LocalDate; 
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import java.time.LocalDate;
 import jakarta.validation.Valid;
 import java.util.List;
 
@@ -76,6 +76,7 @@ public class FilmController {
     }
 }
 }
+
 
 
 
