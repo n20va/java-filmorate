864af72b6e60c1ab73b4e18bc34c09b09dcbbc05
n20va <99407852+n20va@users.noreply.github.com>
2025-09-09 22:30:16 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,30 +1,119 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
-import lombok.Data;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
-@Data
-@Builder
 public class Film {
     private int id;
-
+    
     @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
-
-    @Size(max = 200, message = "Описание фильма не может превышать 200 символов")
+    
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
     private String description;
-
-    @NotNull(message = "Дата релиза не может быть пустой")
+    
     private LocalDate releaseDate;
-
-    @Positive(message = "Продолжительность фильма должна быть положительной")
+    
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
     private int duration;
+    
+    private Set<Integer> likes = new HashSet<>();
+    
+    public Film() {
+    }
+    
+    public Film(int id, String name, String description, LocalDate releaseDate, int duration) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+    
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    
+    public int getDuration() {
+        return duration;
+    }
+    
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    public Set<Integer> getLikes() {
+        return likes;
+    }
+    
+    public void setLikes(Set<Integer> likes) {
+        this.likes = likes;
+    }
+    
+    public void addLike(int userId) {
+        likes.add(userId);
+    }
+    
+    public void removeLike(int userId) {
+        likes.remove(userId);
+    }
+    
+    public int getLikesCount() {
+        return likes.size();
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return id == film.id;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    
+    @Override
+    public String toString() {
+        return "Film{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", duration=" + duration +
+                ", likes=" + likes +
+                '}';
+    }
 }
-
-
-
