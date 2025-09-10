8f3366412ddb3137bb36dabb8e2e10de26f25d97
n20va <99407852+n20va@users.noreply.github.com>
2025-09-10 21:34:38 +0300

    Update UserService.java

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,13 +1,16 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class UserService {
     private final UserStorage userStorage;
@@ -18,13 +21,22 @@ public class UserService {
         this.userStorage = userStorage;
     }
 
+    private User getUserOrThrow(int userId) {
+        return userStorage.getUserById(userId)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id=" + userId + " не найден"));
+    }
+
     public User addUser(User user) {
-        return userStorage.addUser(user);
+        User addedUser = userStorage.addUser(user);
+        log.info("Добавлен пользователь: {}", addedUser);
+        return addedUser;
     }
 
     public User updateUser(User user) {
-        getUserById(user.getId());
-        return userStorage.updateUser(user);
+        getUserOrThrow(user.getId());
+        User updatedUser = userStorage.updateUser(user);
+        log.info("Обновлен пользователь: {}", updatedUser);
+        return updatedUser;
     }
 
     public List<User> getAllUsers() {
@@ -32,50 +44,51 @@ public class UserService {
     }
 
     public User getUserById(int id) {
-        User user = userStorage.getUserById(id);
-        if (user == null) {
-            throw new NotFoundException("Пользователь с id=" + id + " не найден");
-        }
-        return user;
+        return getUserOrThrow(id);
     }
 
     public void addFriend(int userId, int friendId) {
-        getUserById(userId);
-        getUserById(friendId);
-
+        if (userId == friendId) {
+            throw new ValidationException("Нельзя добавить самого себя в друзья");
+        }
+        getUserOrThrow(userId);
+        getUserOrThrow(friendId);
         friends.computeIfAbsent(userId, k -> new HashSet<>()).add(friendId);
         friends.computeIfAbsent(friendId, k -> new HashSet<>()).add(userId);
+        log.info("Пользователь с id={} добавил в друзья пользователя с id={}", userId, friendId);
     }
 
     public void removeFriend(int userId, int friendId) {
-        getUserById(userId);
-        getUserById(friendId);
+        if (userId == friendId) {
+            throw new ValidationException("Нельзя удалить самого себя из друзей");
+        }
+        getUserOrThrow(userId);
+        getUserOrThrow(friendId);
         if (friends.containsKey(userId)) {
             friends.get(userId).remove(friendId);
         }
         if (friends.containsKey(friendId)) {
             friends.get(friendId).remove(userId);
         }
+        log.info("Пользователь с id={} удалил из друзей пользователя с id={}", userId, friendId);
     }
 
     public List<User> getFriends(int userId) {
-        getUserById(userId);
         if (!friends.containsKey(userId)) {
             return new ArrayList<>();
         }
         return friends.get(userId).stream()
-                .map(this::getUserById)
+                .map(this::getUserOrThrow)
                 .collect(Collectors.toList());
     }
 
     public List<User> getCommonFriends(int userId, int otherId) {
         Set<Integer> userFriends = friends.getOrDefault(userId, new HashSet<>());
         Set<Integer> otherFriends = friends.getOrDefault(otherId, new HashSet<>());
-
+        
         return userFriends.stream()
                 .filter(otherFriends::contains)
-                .map(this::getUserById)
+                .map(this::getUserOrThrow)
                 .collect(Collectors.toList());
     }
 }
-
