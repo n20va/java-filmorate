ae8e4d91f060cc836dac94c678f2a8d9f36da5dc
n20va <99407852+n20va@users.noreply.github.com>
2025-09-10 21:42:45 +0300

    Update UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -2,13 +2,11 @@ package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserStorage {
     User addUser(User user);
-
     User updateUser(User user);
-
     List<User> getAllUsers();
-
-    User getUserById(int id);
+    Optional<User> getUserById(int id);
 }
