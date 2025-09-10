ebd514caa4604bfe3a8903e4d05e1dfd77726598
n20va <99407852+n20va@users.noreply.github.com>
2025-09-10 21:36:21 +0300

    Update GlobalExceptionHandler.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,15 +24,6 @@ public class GlobalExceptionHandler {
     public ErrorResponse handleValidationException(final ValidationException e) {
         log.warn("ValidationException: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        String errorMessage = e.getFieldError() != null ?
-            e.getFieldError().getDefaultMessage() : "Validation failed";
-        log.warn("MethodArgumentNotValidException: {}", errorMessage);
-        return new ErrorResponse(errorMessage);
     }
 
     @ExceptionHandler(Throwable.class)
