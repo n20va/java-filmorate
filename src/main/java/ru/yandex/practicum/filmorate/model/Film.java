a7bac39601e0e2209f1639cd3fa5c682f92c3713
n20va <99407852+n20va@users.noreply.github.com>
2025-09-10 21:31:34 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,158 +1,17 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Objects;
 
+@Getter
+@Setter
+@Builder
 public class Film {
     private int id;
     private String name;
     private String description;
     private LocalDate releaseDate;
     private int duration;
-    private Set<Integer> likes = new HashSet<>();
-
-    public Film() {
-    }
-
-    public Film(int id, String name, String description, LocalDate releaseDate, int duration) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-    }
-
-    public static FilmBuilder builder() {
-        return new FilmBuilder();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public Set<Integer> getLikes() {
-        return likes;
-    }
-
-    public void setLikes(Set<Integer> likes) {
-        this.likes = likes;
-    }
-
-    public void addLike(int userId) {
-        likes.add(userId);
-    }
-
-    public void removeLike(int userId) {
-        likes.remove(userId);
-    }
-
-    public int getLikesCount() {
-        return likes.size();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Film film = (Film) o;
-        return id == film.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Film{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", releaseDate=" + releaseDate +
-                ", duration=" + duration +
-                ", likes=" + likes +
-                '}';
-    }
-
-    public static class FilmBuilder {
-        private int id;
-        private String name;
-        private String description;
-        private LocalDate releaseDate;
-        private int duration;
-
-        public FilmBuilder id(int id) {
-            this.id = id;
-            return this;
-        }
-
-        public FilmBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public FilmBuilder description(String description) {
-            this.description = description;
-            return this;
-        }
-
-        public FilmBuilder releaseDate(LocalDate releaseDate) {
-            this.releaseDate = releaseDate;
-            return this;
-        }
-
-        public FilmBuilder duration(int duration) {
-            this.duration = duration;
-            return this;
-        }
-
-        public Film build() {
-            Film film = new Film();
-            film.setId(id);
-            film.setName(name);
-            film.setDescription(description);
-            film.setReleaseDate(releaseDate);
-            film.setDuration(duration);
-            return film;
-        }
-    }
 }
