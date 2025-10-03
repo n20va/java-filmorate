437ad45f20ba763c4dbb01ac250cc38a76ce8823
n20va <99407852+n20va@users.noreply.github.com>
2025-10-03 23:10:28 +0300

    Update InMemoryUserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 import java.util.*;
 
-@Component
+@Component("inMemoryUserStorage")
 public class InMemoryUserStorage implements UserStorage {
     private final Map<Integer, User> users = new HashMap<>();
+    private final Map<Integer, Set<Integer>> friends = new HashMap<>();
     private int nextId = 1;
 
     @Override
@@ -28,8 +29,50 @@ public class InMemoryUserStorage implements UserStorage {
     }
 
     @Override
-public Optional<User> getUserById(int id) {
-    return Optional.ofNullable(users.get(id));
-}
+    public Optional<User> getUserById(int id) {
+        return Optional.ofNullable(users.get(id));
+    }
 
+    @Override
+    public void addFriend(int userId, int friendId) {
+        friends.computeIfAbsent(userId, k -> new HashSet<>()).add(friendId);
+        friends.computeIfAbsent(friendId, k -> new HashSet<>()).add(userId);
+    }
+
+    @Override
+    public void removeFriend(int userId, int friendId) {
+        if (friends.containsKey(userId)) {
+            friends.get(userId).remove(friendId);
+        }
+        if (friends.containsKey(friendId)) {
+            friends.get(friendId).remove(userId);
+        }
+    }
+
+    @Override
+    public List<User> getFriends(int userId) {
+        if (!friends.containsKey(userId)) {
+            return new ArrayList<>();
+        }
+        List<User> friendList = new ArrayList<>();
+        for (Integer friendId : friends.get(userId)) {
+            getUserById(friendId).ifPresent(friendList::add);
+        }
+        return friendList;
+    }
+
+    @Override
+    public List<User> getCommonFriends(int userId, int otherId) {
+        Set<Integer> userFriends = friends.getOrDefault(userId, new HashSet<>());
+        Set<Integer> otherFriends = friends.getOrDefault(otherId, new HashSet<>());
+        
+        Set<Integer> commonFriendIds = new HashSet<>(userFriends);
+        commonFriendIds.retainAll(otherFriends);
+        
+        List<User> commonFriends = new ArrayList<>();
+        for (Integer friendId : commonFriendIds) {
+            getUserById(friendId).ifPresent(commonFriends::add);
+        }
+        return commonFriends;
+    }
 }
