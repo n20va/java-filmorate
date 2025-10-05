487ae8939f11de354beab0021bb685942efff785
n20va <99407852+n20va@users.noreply.github.com>
2025-10-05 21:14:28 +0300

    Update InMemoryFilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -1,11 +1,12 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Film;
-import java.util.*;
 
-@Component("inMemoryFilmStorage")
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Repository("inMemoryFilmStorage")
 public class InMemoryFilmStorage implements FilmStorage {
     private final Map<Integer, Film> films = new HashMap<>();
     private int nextId = 1;
@@ -34,7 +35,26 @@ public class InMemoryFilmStorage implements FilmStorage {
     }
 
     @Override
-    public RowMapper<Film> getFilmRowMapper() {
-        throw new UnsupportedOperationException("RowMapper not supported for in-memory storage");
+    public void addLike(int filmId, int userId) {
+        Film film = films.get(filmId);
+        if (film != null) {
+            film.getLikes().add(userId);
+        }
+    }
+
+    @Override
+    public void removeLike(int filmId, int userId) {
+        Film film = films.get(filmId);
+        if (film != null) {
+            film.getLikes().remove(userId);
+        }
+    }
+
+    @Override
+    public List<Film> getPopularFilms(int count) {
+        return films.values().stream()
+                .sorted((f1, f2) -> Integer.compare(f2.getLikes().size(), f1.getLikes().size()))
+                .limit(count)
+                .collect(Collectors.toList());
     }
 }
