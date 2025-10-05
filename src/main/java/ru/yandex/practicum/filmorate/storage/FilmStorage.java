00c01001c2705180bb366fbb59426a6eb2c21ffd
n20va <99407852+n20va@users.noreply.github.com>
2025-10-05 21:05:15 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,11 +5,18 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     List<Film> getAllFilms();
+
     Optional<Film> getFilmById(int id);
+
     void addLike(int filmId, int userId);
+
     void removeLike(int filmId, int userId);
+
     List<Film> getPopularFilms(int count);
 }
