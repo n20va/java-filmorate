fb4666812cca0754597f7266ada81df60a3d5b84
n20va <99407852+n20va@users.noreply.github.com>
2025-10-05 20:54:04 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,20 +1,15 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Film;
-import org.springframework.jdbc.core.RowMapper;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
-
     Film addFilm(Film film);
-
     Film updateFilm(Film film);
-
     List<Film> getAllFilms();
-
     Optional<Film> getFilmById(int id);
-
-    RowMapper<Film> getFilmRowMapper();
+    void addLike(int filmId, int userId);
+    void removeLike(int filmId, int userId);
+    List<Film> getPopularFilms(int count);
 }
