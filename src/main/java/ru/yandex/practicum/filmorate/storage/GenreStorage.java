6c3043370e29c86bb3940990df7ce059c61fa5f7
n20va <99407852+n20va@users.noreply.github.com>
2025-10-06 10:20:18 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import java.util.List;
+import java.util.Optional;
+
+public interface GenreStorage {
+
+    List<Genre> getAllGenres();
+
+    Optional<Genre> getGenreById(int id);
+
+    void loadGenresForFilms(List<Film> films);
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaStorage.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.MpaRating;
+import java.util.List;
+import java.util.Optional;
+
+public interface MpaStorage {
+
+    List<MpaRating> getAllMpa();
+
+    Optional<MpaRating> getMpaById(int id);
+}
