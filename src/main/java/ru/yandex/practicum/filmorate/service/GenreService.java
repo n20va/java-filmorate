f27c939e185ef620482f72e648299907e7f7e740
n20va <99407852+n20va@users.noreply.github.com>
2025-10-06 10:21:08 +0300

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.storage.GenreStorage;
+
+import java.util.List;
+
+@Service
+public class GenreService {
+
+    private final GenreStorage genreStorage;
+
+    @Autowired
+    public GenreService(GenreStorage genreStorage) {
+        this.genreStorage = genreStorage;
+    }
+
+    public List<Genre> getAllGenres() {
+        return genreStorage.getAllGenres();
+    }
+
+    public Genre getGenreById(int id) {
+        return genreStorage.getGenreById(id)
+                .orElseThrow(() -> new NotFoundException("Жанр с id=" + id + " не найден"));
+    }
+
+    public void loadGenresForFilms(List<Film> films) {
+        genreStorage.loadGenresForFilms(films);
+    }
+}
